0beaf42d3697928c70f19c699e238f297a504605
efigueirasg <efigueirasg@gmail.com>
2023-10-29 16:39:05 +0100

    aplicar estilo a la pagina de  receta

--- frontend/app/components/RecipeDetail.js
+++ frontend/app/components/RecipeDetail.js
@@ -1,12 +1,40 @@
 import React from 'react';
-
+import "../styles/recipeDetail.css";
 
 const RecipeDetail = ({ recipeData }) => {
   return (
     <div>
-        <div>
-            {recipeData.recipe ? <h1>{recipeData.recipe.title}</h1> : <h1></h1>}
+      <div className='info-general'>
+        {recipeData.recipe && (
+          <div className='info-img'>
+            <img src={recipeData.recipe.recipe_picture} alt={recipeData.recipe.title} />
+          </div>
+        )}
+        <div className='info-basic'>
+          <div>
+            {recipeData.recipe && <h1 className='name-recipe'>{recipeData.recipe.title}</h1>}
+          </div>
+          <div className='time-servings'>
+            <div className='servings'>
+              <img src='../cubiertos.png' />
+              <p>{recipeData.recipe && <span >{recipeData.recipe.servings}</span>} raciones</p>
+            </div>
+            <div className='time'>
+              <img src='../reloj.png' />
+              <p>{recipeData.recipe && <span >{recipeData.recipe.cook_time}</span>}  min</p>
+            </div>
+          </div>
         </div>
+      </div>
+      <div className='ingredients-preparation'>
+        <div className='ingredients'>
+          <h2 className='name-ig-pr'>INGREDIENTES   </h2>
+        </div>
+        <div className='preparation'>
+          <h2 className='name-ig-pr'>PREPARACIÓN</h2>
+          <p className='preparation-data'>{recipeData.recipe && <span >{recipeData.recipe.description}</span>}</p>
+        </div>
+      </div>
     </div>
   );
 };
