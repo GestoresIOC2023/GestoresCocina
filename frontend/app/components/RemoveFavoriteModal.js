9a0e597eef7196e7359727eb1733d2f03e954aef
efigueirasg <efigueirasg@gmail.com>
2023-11-21 15:52:46 +0100

    estilo modal eliminar receta favorita

--- frontend/app/components/RemoveFavoriteModal.js
+++ frontend/app/components/RemoveFavoriteModal.js
@@ -18,21 +18,65 @@ const RemoveFavoriteModal = ({ open, onClose, onConfirm }) => {
           top: "50%",
           left: "50%",
           transform: "translate(-50%, -50%)",
-          width: 400,
-          bgcolor: "background.paper",
-          border: "2px solid #000",
-          boxShadow: 24,
-          p: 4,
+          width: 300,
+          bgcolor: "#F6E9E0",
+          borderRadius: "8px",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+          p: 3,
         }}
       >
-        <Typography id="modal-title" variant="h6" component="h2">
+        <Typography
+          id="modal-title"
+          variant="h6"
+          component="h2"
+          sx={{ color: "#FF6724", textAlign: "center", mb: 2 }}
+        >
           Confirmar eliminación
         </Typography>
-        <Typography id="modal-description" sx={{ mt: 2 }}>
+        <Typography
+          id="modal-description"
+          sx={{ textAlign: "center", color: "#502918", mb: 2 }}
+        >
           ¿Estás seguro de que quieres eliminar esta receta de favoritos?
         </Typography>
-        <Button onClick={onConfirm}>Aceptar</Button>
-        <Button onClick={onClose}>Cancelar</Button>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-around",
+            mt: 2,
+          }}
+        >
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: "#FF6724",
+              color: "#F6E9E0",
+              "&:hover": {
+                backgroundColor: "transparent",
+                color: "#FF6724",
+                border: "1px solid #FF6724",
+              },
+            }}
+            onClick={onConfirm}
+          >
+            Aceptar
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: "#502918",
+              color: "#F6E9E0",
+              "&:hover": {
+                backgroundColor: "transparent",
+                color: "#502918",
+                border: "1px solid #502918",
+              },
+            }}
+            onClick={onClose}
+          >
+            Cancelar
+          </Button>
+        </Box>
       </Box>
     </Modal>
   );
