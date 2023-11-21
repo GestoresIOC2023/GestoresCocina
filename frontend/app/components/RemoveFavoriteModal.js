2cd6f9ee2c3197f32760888f725b7cbdec609ee6
efigueirasg <efigueirasg@gmail.com>
2023-11-21 16:06:58 +0100

    fixed bug color botones modal

--- frontend/app/components/RemoveFavoriteModal.js
+++ frontend/app/components/RemoveFavoriteModal.js
@@ -5,81 +5,81 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
 const RemoveFavoriteModal = ({ open, onClose, onConfirm }) => {
-  return (
-    <Modal
-      open={open}
-      onClose={onClose}
-      aria-labelledby="modal-title"
-      aria-describedby="modal-description"
-    >
-      <Box
-        sx={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          width: 300,
-          bgcolor: "#F6E9E0",
-          borderRadius: "8px",
-          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-          p: 3,
-        }}
-      >
-        <Typography
-          id="modal-title"
-          variant="h6"
-          component="h2"
-          sx={{ color: "#FF6724", textAlign: "center", mb: 2 }}
+    return (
+        <Modal
+            open={open}
+            onClose={onClose}
+            aria-labelledby="modal-title"
+            aria-describedby="modal-description"
         >
-          Confirmar eliminación
-        </Typography>
-        <Typography
-          id="modal-description"
-          sx={{ textAlign: "center", color: "#502918", mb: 2 }}
-        >
-          ¿Estás seguro de que quieres eliminar esta receta de favoritos?
-        </Typography>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-around",
-            mt: 2,
-          }}
-        >
-          <Button
-            variant="contained"
-            sx={{
-              backgroundColor: "#FF6724",
-              color: "#F6E9E0",
-              "&:hover": {
-                backgroundColor: "transparent",
-                color: "#FF6724",
-                border: "1px solid #FF6724",
-              },
-            }}
-            onClick={onConfirm}
-          >
-            Aceptar
-          </Button>
-          <Button
-            variant="contained"
-            sx={{
-              backgroundColor: "#502918",
-              color: "#F6E9E0",
-              "&:hover": {
-                backgroundColor: "transparent",
-                color: "#502918",
-                border: "1px solid #502918",
-              },
-            }}
-            onClick={onClose}
-          >
-            Cancelar
-          </Button>
-        </Box>
-      </Box>
-    </Modal>
-  );
+            <Box
+                sx={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    width: 300,
+                    bgcolor: "#F6E9E0",
+                    borderRadius: "8px",
+                    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+                    p: 3,
+                }}
+            >
+                <Typography
+                    id="modal-title"
+                    variant="h6"
+                    component="h2"
+                    sx={{ color: "#FF6724", textAlign: "center", mb: 2 }}
+                >
+                    Confirmar eliminación
+                </Typography>
+                <Typography
+                    id="modal-description"
+                    sx={{ textAlign: "center", color: "#502918", mb: 2 }}
+                >
+                    ¿Estás seguro de que quieres eliminar esta receta de favoritos?
+                </Typography>
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "space-around",
+                        mt: 2,
+                    }}
+                >
+                    <Button
+                        variant="contained"
+                        sx={{
+                            backgroundColor: "#FF6724",
+                            color: "#F6E9E0",
+                            "&:hover": {
+                                backgroundColor: "transparent",
+                                color: "#FF6724",
+                                border: "1px solid #FF6724",
+                            },
+                        }}
+                        onClick={onConfirm}
+                    >
+                        Aceptar
+                    </Button>
+                    <Button
+                        variant="contained"
+                        sx={{
+                            backgroundColor: "#502918",
+                            color: "#F6E9E0",
+                            "&:hover": {
+                                backgroundColor: "transparent",
+                                color: "#502918",
+                                border: "1px solid #502918",
+                            },
+                        }}
+                        onClick={onClose}
+                    >
+                        Cancelar
+                    </Button>
+                </Box>
+            </Box>
+        </Modal>
+    );
 };
 
 export default RemoveFavoriteModal;
